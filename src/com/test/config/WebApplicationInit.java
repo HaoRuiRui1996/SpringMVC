e9aa8d59caf015ebfc96eb8b39afbb218ad4013c
HaoRuiRui1996 <1450289924@qq.com>
2018-10-25 18:38:05 +0800

    上传本地代码

---
+++ src/com/test/config/WebApplicationInit.java
@@ -0,0 +1,20 @@
+package com.test.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebApplicationInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] {RootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
